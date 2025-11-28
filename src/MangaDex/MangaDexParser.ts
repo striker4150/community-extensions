590ee2b8b77dc759481d2884d05894d4db1157df
CatalystStrike <darrenmkam4150@gmail.com>
2025-11-27 18:05:04 -1000

    Add fix for MangaDex extension by OTCompa

--- src/MangaDex/MangaDexParser.ts
+++ src/MangaDex/MangaDexParser.ts
@@ -9,7 +9,9 @@ export const parseMangaList = async (object: MangaItem[], source: any, thumbnail
     for (const manga of object) {
         const mangaId = manga.id
         const mangaDetails = manga.attributes
-        const title = source.decodeHTMLEntity(mangaDetails.title.en ?? mangaDetails.altTitles.map(x => Object.values(x).find((v) => v !== undefined)).find((t) => t !== undefined))
+        // Modifications Copyright (c) 2025 OTCompa
+        // 2025-11-18: Fix bug in Mangadex title parser
+        const title = source.decodeHTMLEntity(mangaDetails.title.en ?? Object.values(mangaDetails.title)[0] ?? mangaDetails.altTitles.map(x => Object.values(x).find((v) => v !== undefined)).find((t) => t !== undefined)) ?? 'INVALID TITLE'
         const coverFileName = manga.relationships.filter((x) => x.type == 'cover_art').map((x) => x.attributes?.fileName)[0]
         const image = coverFileName ? `${source.COVER_BASE_URL}/${mangaId}/${coverFileName}${MDImageQuality.getEnding(await thumbnailSelector(source.stateManager))}` : 'https://mangadex.org/_nuxt/img/cover-placeholder.d12c3c5.jpg'
         const subtitle = `${mangaDetails.lastVolume ? `Vol. ${mangaDetails.lastVolume}` : ''} ${mangaDetails.lastChapter ? `Ch. ${mangaDetails.lastChapter}` : ''}`
